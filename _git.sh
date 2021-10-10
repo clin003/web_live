73895d7a9f1b056db61e096a6eccbdf65a283eba
白菜林 <clin003@vip.qq.com>
2021-10-10 22:44:21 +0800

    debug

---
+++ _git.sh
@@ -0,0 +1,5 @@
+git add .
+git commit -m "debug"
+git push
+
+git push -u gitee main
---
+++ _git_init.sh
@@ -0,0 +1,4 @@
+git clone git@github.com:clin003/web_live.git
+cd web_live
+git remote add gitee git@gitee.com:lyhuilin/web_live.git
+git push -u gitee main
